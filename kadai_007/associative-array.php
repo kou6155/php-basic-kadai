05660d72575f0fea107d5987ff426a9df1bdda96
kou6155 <value_of_miracle_0505@yahoo.co.jp>
2025-01-22 18:37:39 +0900

    kadai_007up

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+   <meta charset="utf-8">
+   <title>7章課題</title>
+</head>
+
+<body>
+  <p>
+    <?php
+      $ingredients_data = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+      print_r($ingredients_data);
+    ?>
+  </p>
+</body>
+
+</html>
